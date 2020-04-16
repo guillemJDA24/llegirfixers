c7ebd59a7be55eac58fa0ebc104162e8447a8d51
guillemJDA24 <guillem.perez@iesjoandaustria.org>
2020-04-16 09:01:53 +0000

    ex4

--- noms.py
+++ noms.py
@@ -1,6 +1,14 @@
 #Definiu la variable que ens guardarà l'estadística per noms.
+fitxer = open("noms.txt",'r')
 #Pista clau ha de ser el nom i valor els cops que apareix. 
-
+diccionari={}
+for n in fitxer:
+  if n in diccionari:
+    diccionari[n]=diccionari[n]+1
+  else:
+    diccionari[n]=1
+print(diccionari)
+  
 #Heu de llegir el fitxer noms.
 
 #Aneu llegint  línia a lína que correspon a un nom.
@@ -8,5 +16,5 @@
 
 #Finalment pinteu tots els noms i amb els cops que s'ha posat el darrer any.
 #Pintar només la variable. Els diccionaris ja surten ordenats alfabèticament.
-
+#diccionari[clau]=valor
 
